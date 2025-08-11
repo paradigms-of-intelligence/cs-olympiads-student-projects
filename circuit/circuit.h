a9a5f253042e444ece601059015883b6c651ff2b
lamatematica14 <sofia.marago@gmail.com>
2025-08-11 15:59:30 +0200

    Code cleanup

--- circuit/circuit.h
+++ circuit/circuit.h
@@ -4,9 +4,67 @@
 #include "string"
 using std::string;
 
+#ifndef INT32_MAX
+typedef int int32_t;
+#endif
+
 const string networkinputfile = "network";
 const string baseinputfile = "./testdata/decompled/img_";
 const int INPUT_NODES = 784;
 const int one = 2147483647, zero = 2147483646;
 const int TESTS = 100;
+
+#pragma region declarations
+
+//Beginning of declarations
+enum
+{
+    EXIT_WRONG_USAGE = 1,
+    EXIT_FILE_ERROR = 2,
+    EXIT_INVALID_NODE_DATA = 3,
+    EXIT_TOPOSORT_FAILED = 4
+};
+
+
+void __program_abort(size_t exit_code)
+{
+    std::printf("Error code: %d", exit_code);
+    switch (exit_code)
+    {
+    case EXIT_WRONG_USAGE:
+        std::printf("Usage: convert_network <path_to_type16>");
+        break;
+    case EXIT_FILE_ERROR:
+        std::printf("Error in I/O processing.");
+        break;
+    case EXIT_INVALID_NODE_DATA:
+        std::printf("Invalid node ID.");
+        break;
+    case EXIT_TOPOSORT_FAILED:
+        std::printf("Toposort failed.");
+        break;
+    default:
+        std::printf("Error. No additional information supplied.");
+        break;
+    }
+
+    exit(1);
+}
+
+inline int32_t __read_int32_t(std::ifstream &in)
+{
+    int32_t result = 0;
+    try
+    {
+        in.read(reinterpret_cast<char*>(&result), 4);
+        if(in.fail()) throw std::runtime_error("Reading failed");
+    }
+    catch(const std::exception& e)
+    {
+        std::printf(e.what());
+        __program_abort(EXIT_FILE_ERROR);
+    }
+    return result;
+}
+
 #endif
--- circuit/evaluator/evaluator.cpp
+++ circuit/evaluator/evaluator.cpp
@@ -2,17 +2,6 @@
 #include "circuit.h"
 using namespace std;
 
-
-// int reading from binary file
-int readint(ifstream &stream) {
-    char * buffer = new char [32];
-    stream.read((char*)&buffer, 4);
-    int r = strtol(buffer, &buffer, 2);
-    delete[] buffer;
-    //cout << r << " DEGUB" << endl;
-    return r;
-}
-
 struct AndNot_network {
     int N;
     vector<bool> value; //bit values for calculation
@@ -22,17 +11,17 @@ struct AndNot_network {
     void init() {
         // initialize current network
         ifstream graphinput(networkinputfile, ios::binary);
-        N = readint(graphinput)+1;
+        N = __read_int32_t(graphinput)+1;
         value.resize(N); C_1.resize(N); C_2.resize(N);
 
         for (int i = 0; i < N; i++) {
-            int id = readint(graphinput);
-            C_1[id] = readint(graphinput);
-            C_2[id] = readint(graphinput);
+            int id = __read_int32_t(graphinput);
+            C_1[id] = __read_int32_t(graphinput);
+            C_2[id] = __read_int32_t(graphinput);
         }
 
         result_nodes.resize(10);
-        for (int i = 0; i < 10; i++) result_nodes[i] = readint(graphinput);
+        for (int i = 0; i < 10; i++) result_nodes[i] = __read_int32_t(graphinput);
         graphinput.close();
     }
 
