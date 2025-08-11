b97b9ae9d26c0545546a4e6a31a79b3f5350f838
meteoritini <60318593+Meteoritini@users.noreply.github.com>
2025-08-11 15:20:36 +0200

    Decompiled t10k data

---
+++ circuit/data/extract.py
@@ -0,0 +1,31 @@
+import gzip
+import numpy as np
+
+dotest = input("Decompile test data instead? y/N : ").lower() == 'y'
+
+
+images = gzip.open('t10k-images-idx3-ubyte.gz' if dotest else 'train-images-idx3-ubyte.gz','r')
+labels = gzip.open('t10k-images-idx3-ubyte.gz' if dotest else 'train-labels-idx1-ubyte.gz','r')
+labels.read(8)
+images.read(16)
+
+image_size = 28
+buf = images.read()
+
+num_images = int(len(buf)/(image_size**2))
+print(f"Number of images: {num_images}")
+
+data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+data = data.reshape(num_images, image_size, image_size, 1)
+
+imgid = 0
+
+for image in data:
+    with open(f"{'testdata' if dotest else 'training'}/img_{imgid}.txt", "w") as file:
+        imgid += 1
+        for row in image:
+            for p in row:
+                file.write("0" if p < 128 else "1")
+            pass
+        buf = labels.read(1)
+        file.write(f"\n{str(np.frombuffer(buf, dtype=np.uint8).astype(np.int64))[1]}") 
