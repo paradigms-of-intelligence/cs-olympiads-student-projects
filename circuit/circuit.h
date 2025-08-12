93a214a077d9393d139e47eb42805d116c4d1e47
acoatoitgs <andrea.coato@gmail.com>
2025-08-12 09:28:16 +0200

    fixed circuit.h pragma

--- circuit/circuit.h
+++ circuit/circuit.h
@@ -1,5 +1,5 @@
-#ifndef _H_CIRCUIT
-#define _H_CIRCUIT
+#ifndef CIRCUIT_H
+#define CIRCUIT_H
 
 #include "string"
 using std::string;
@@ -23,7 +23,6 @@ enum
     EXIT_CONVERSION_ERROR = 6
 };
 
-#pragma region declarations
 
 inline int32_t read_int32_t(std::ifstream &in);
 inline void write_int32_t(std::ofstream &out, int32_t data);
