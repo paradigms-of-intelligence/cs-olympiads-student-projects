562ddf3b7e407c5a5b4f2bb2b5cfad05a1f7a390
acoatoitgs <andrea.coato@gmail.com>
2025-08-18 11:21:33 +0200

    tweaked values

--- circuit/network/main.py
+++ circuit/network/main.py
@@ -43,8 +43,8 @@ BATCH_SIZE = 1000
 BETA2 = .99
 BETA1 = .9
 EPSILON = 1e-8
-LEARNING_RATE = 0.05
-LEARNING_INCREASE = 1.05
+LEARNING_RATE = 0.03
+LEARNING_INCREASE = 1.02
 
 # This should be multiplied by BETA1 and BETA2
 # and be updated for each iteration
